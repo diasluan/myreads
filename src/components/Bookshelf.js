241065ac56ef220e0e4b83cab65646661557ef5f
Luan Dias <luan.dias@rockcontent.com>
2018-12-15 22:04:50 -0200

    Add Bookshelf component

---
+++ src/components/Bookshelf.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Book from './Book'
+
+class Bookshelf extends Component {
+  static PropTypes = {
+    title: PropTypes.string.isRequired,
+    books: PropTypes.array,
+    changeShelf: PropTypes.func.isRequired
+  }
+
+  render() {
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{ this.props.title }</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            {this.props.books.map((book) => (
+              <li key={book.id}>
+                <Book title={book.title} 
+                      author={book.author} 
+                      cover={book.cover} 
+                      changeShelf={this.props.changeShelf} />
+              </li>
+            ))
+            }
+          </ol>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Bookshelf
